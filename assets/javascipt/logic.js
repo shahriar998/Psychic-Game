5b8441ad97038a851e1695d6103435c2970b942f
Shahriar Elias <shahriar998@gmail.com>
2019-06-15 02:38:30 -0400

    working code

--- assets/javascipt/logic.js
+++ assets/javascipt/logic.js
@@ -1,37 +1,99 @@
 //Gave variable names which will be displayed 
-var win =0;
-var loss =0;
+var win = 0;
+var loss = 0;
 var guessLeft = 9;
 var guessed = [];
 
-var computerOptions= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //options available for computer 
-console.log(computerOptions.length);
+// console.log(computerOptions.length);
 
 //to check computer is picking random number between 26 and displaying random aplhabet from the array
-var randomNumber = Math.floor(Math.random()*26);
-var randomNumber1= randomNumber-1;
-console.log(randomNumber);
+var randomNumber = Math.floor(Math.random() * 26);
+var randomNumber1 = randomNumber - 1;
+// console.log(randomNumber);
 console.log(randomNumber1);
 var comGuessedAlpha = computerOptions[randomNumber1];
 console.log(comGuessedAlpha);
 
 //user guessed alphabet
 
-document.onkeyup = function(event){
-    var keyPressed =event.key;
+document.onkeyup = function (event) {
+    var keyPressed = event.key;
     console.log(keyPressed);
-    guessed.push(keyPressed);
+    
+    //guessLeftNumber.textContent = guessLeft;
+    //guessed.push(keyPressed);
     console.log(guessed);
+
+
+
+
+    // Create variables that hold references to the places in the HTML where we want to display things.
+    var winNumber = document.getElementById("user-win");
+    var lossNumber = document.getElementById("user-lose");
+    var guessLeftNumber = document.getElementById("user-guess");
+    var choosenAlphabet = document.getElementById("guessed-letter");
+    // var computerAlphabet = document.getElementById("computer-guess");
+
+    //condition
+
+    if (keyPressed === comGuessedAlpha) {
+
+        win++;
+        guessed.push(keyPressed);
+        choosenAlphabet.textContent=guessed;
+
+        randomNumber = Math.floor(Math.random() * 26);
+        randomNumber1 = randomNumber - 1;
+        comGuessedAlpha = computerOptions[randomNumber1];
+        console.log(comGuessedAlpha);
+        guessed = [];
+        guessLeft = 9;
+        guessLeftNumber.textContent = guessLeft;
+        
+        // computerAlphabet.textContent = comGuessedAlpha;
+
+
+    }
+    else {
+
+        
+        if (keyPressed !== comGuessedAlpha) {
+
+            guessed.push(keyPressed);
+            guessLeft--;
+            guessLeftNumber.textContent = guessLeft;
+            // console.log(guessLeft);
+            choosenAlphabet.textContent = guessed;
+            // computerAlphabet.textContent = comGuessedAlpha;
+            console.log(comGuessedAlpha);
+            if (guessLeft === 0) {
+                loss++
+                // console.log(guessLeft);
+                randomNumber = Math.floor(Math.random() * 26);
+                randomNumber1 = randomNumber - 1;
+                comGuessedAlpha = computerOptions[randomNumber1];
+                lossNumber.textContent = loss;
+                guessLeft = 9;
+                guessLeftNumber.textContent = guessLeft;
+                guessed = [];
+                choosenAlphabet.textContent = guessed;
+                // computerAlphabet.textContent = comGuessedAlpha;
+                console.log(comGuessedAlpha);
+            }
+            
+         
+            // computerAlphabet.textContent=comGuessedAlpha;
+            //  choosenAlphabet.textContent=guessed;
+            
+
+        }
+        
+        
+    }
+
+
+
+    winNumber.textContent = win;
 };
-
-
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var winNumber = document.getElementById("user-win");
-var lossNumber = document.getElementById("user-lose");
-var guessLeftNumber = document.getElementById("user-guess");
-var choosenAlphabet = document.getElementById("guessed-letter");
-var computerAlphabet = document.getElementById("computer-guess");
-
-//condition
